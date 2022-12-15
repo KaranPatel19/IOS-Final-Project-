2e068af21f0950abd484fa4a89feb0260165d83c
KaranPatel19 <41769712+KaranPatel19@users.noreply.github.com>
2022-12-15 13:23:36 -0500

    made design of almost all pages

---
+++ Cambrian Course selection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cambrian Course selection
+//
+//  Created by Karan Patel on 2022-11-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Cambrian Course selection' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Cambrian Course selection
+pod 'dropdown'
+
+end
